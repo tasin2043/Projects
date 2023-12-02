a529146420fe127b6097939dbb4753a0e49892a6
TasinCode <tasin2043@gmail.com>
2023-12-02 15:29:21 +0600

    Project-1 for JS

---
+++ JavaScript/Project-1/script1.js
@@ -0,0 +1,22 @@
+const slideShowElements = document.querySelectorAll(".slideshow-element");
+
+
+let countElements = 1;
+
+setInterval(() => {
+    countElements ++;
+
+    let currentElement = document.querySelector(".current");
+
+    currentElement.classList.remove("current");
+
+    if(countElements > slideShowElements.length){
+        slideShowElements[0].classList.add("current");
+        countElements = 1;
+    }else{
+        currentElement.nextElementSibling.classList.add("current");
+    }
+
+    
+},2000);
+
